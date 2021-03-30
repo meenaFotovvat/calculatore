c3067f64ebdc8c032cf82d8de2a9ca12535d902b
meenaFotovvat <meemafotovvat@gmail.com>
2021-03-30 16:47:52 +0430

    add screen change

--- calculatore.js
+++ calculatore.js
@@ -1,15 +1,28 @@
-const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
+// const calculator = {
+//   displayValue: '0',
+//   firstOperand: null,
+//   waitingForSecondOperand: false,
+//   operator: null,
+// };
 
-function updateDisplay() {
-  // select the element with class of `calculator-screen`
-  const display = document.querySelector('.calculate');
-  // update the value of the element with the contents of `displayValue`
-  display.value = calculator.displayValue;
-}
+// function updateDisplay() {
+//   // select the element with class of `calculator-screen`
+//   const display = document.querySelector('.calculate');
+//   console.log(display)
+//   // update the value of the element with the contents of `displayValue`
+//   display.innerHTML = calculator.displayValue;
+//   console.log(display.value)
+// }
 
-updateDisplay();
+// updateDisplay();
+
+
+let paragraph = document.querySelector('.calculate');
+let buttons = document.querySelectorAll('.col_1');
+
+document.querySelector('.container').addEventListener('click', function(event) {
+  console.log(event.target.innerHTML)
+  paragraph.innerHTML = event.target.innerHTML
+  
+ 
+});
