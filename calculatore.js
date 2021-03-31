35b839a8e0dbb477a1d88f28fea7dd92ff590a23
meenaFotovvat <meemafotovvat@gmail.com>
2021-03-31 16:07:56 +0430

    add displayMode func

--- calculatore.js
+++ calculatore.js
@@ -1,9 +1,8 @@
-// const calculator = {
-//   displayValue: '0',
-//   firstOperand: null,
-//   waitingForSecondOperand: false,
-//   operator: null,
-// };
+const calculator = {
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
 
 // function updateDisplay() {
 //   // select the element with class of `calculator-screen`
@@ -16,13 +15,78 @@
 
 // updateDisplay();
 
+let displayValue = document.querySelector('.screen');
 
-let paragraph = document.querySelector('.calculate');
-let buttons = document.querySelectorAll('.col_1');
+// function displayMode(){
+//   if (displayValue.value === "0"){
+//     displayValue.value = event.target.innerHTML;
+//   } else {
+//     displayValue.value += event.target.innerHTML;
+//   }
+//   console.log(calculator);
 
-document.querySelector('.container').addEventListener('click', function(event) {
-  console.log(event.target.innerHTML)
-  paragraph.innerHTML = event.target.innerHTML
-  
- 
+// }
+
+const name = {
+  value : "pouria"
+}
+
+const {value } = name
+
+function displayMode(digit){
+  const {innerText} = digit.target
+  if (displayValue.value === "0"){
+    displayValue.value = innerText;
+  } else {
+    displayValue.value += innerText;
+  }
+  console.log(displayValue.value);
+
+}
+
+function operatorHandler(nextOperator){
+  const { firstOperand, operator } = calculator;
+   
+  const inputValue = parseFloat(displayValue);
+
+  if (calculator.firstOperand==="null" && !isNaN(inputValue)){
+    calculator.firstOperand = inputValue;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+
+
+}
+
+const keys = document.querySelector('.container');
+keys.addEventListener('click', (event) => {
+  // Access the clicked element
+  const { target } = event;
+  // Check if the clicked element is a button.
+  // If not, exit from the function
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    // operatorHandler();
+    // console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('reset')) {
+    console.log('reset', target.value);
+    return;
+  } 
+
+  if (target.classList.contains('clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  displayMode(event);
+  // displayMode();
+  // console.log('digit', target.value);
 });
+
